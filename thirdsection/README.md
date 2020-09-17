38c0c9dc9958ebf2522d06033376ca8c0ef0b79e
Rishabh Jain <imrj1999@gmail.com>
2020-09-17 19:10:20 +0530

    commit

--- thirdsection/README.md
+++ thirdsection/README.md
@@ -4,3 +4,4 @@ useful commands:
 fs.writeFileSync // to create a file
 fs.appendFileSync('writefile.txt' , 'this is appended ') // to append
 fs.readFileSync('./notes.txt' , {encoding : 'utf-8'})
+chalk module is used to colorify your console statements
--- thirdsection/app.js
+++ thirdsection/app.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
+const chalk = require('chalk');
 const name = require('./name.js')
 const validator = require('validator');
 const getnotes = require('./notes.js');
 
+const error = chalk.bgRedBright.black;
+const success = chalk.bgGreen.black;
 console.log(getnotes);
 
 fs.appendFileSync('writefile.txt' , 'this is appended ');
 console.log(validator.isEmail('imrj1999@gmail.com'))
-
+console.log(success('success'))
